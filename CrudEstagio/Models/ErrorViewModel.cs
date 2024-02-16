f59d108344c0f3ff35f884c2ba1df378b5628a9c
Rafael Mecenas <rafaeljmecenas1@gmail.com>
2024-02-16 16:52:21 -0300

    Adicionar arquivos de projeto.

---
+++ CrudEstagio/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace CrudEstagio.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
